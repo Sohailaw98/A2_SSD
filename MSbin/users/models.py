159d3f45e088612901f43dc8f8d92ce912c24bdf
sohailaw98 <sohailaw98@gmail.com>
2019-03-14 17:52:23 -0400

    -minor issues with registration and login page were fixed

--- MSbin/users/models.py
+++ MSbin/users/models.py
@@ -4,12 +4,12 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='Default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self, **kwargs):
         super().save()
 
         img = Image.open(self.image.path)
